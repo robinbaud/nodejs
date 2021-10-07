47ce39ac7398b787076ad2079723e37251cdc8d8
robinbaud <72597879+robinbaud@users.noreply.github.com>
2021-10-07 13:19:59 +0200

    go

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,72 @@
-const path = require('path');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const MinifyHtmlWebpackPlugin = require("minify-html-webpack-plugin");
+const PurgecssPlugin = require('purgecss-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
+const glob = require('glob')
+
+
+const PATHS = {
+  src: path.join(__dirname, 'src')
+}
 
 module.exports = {
   mode: "production",
+  resolve: {
+    modules: ["node_modules"],
+  },
   entry: {
-    polyfill: "babel-polyfill",
-    app: "./src/index.js"
+    app: "./src/index.js",
   },
   output: {
     filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "public")
+    path: path.resolve(__dirname, "public"),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "/src/html"),
+    },
+    compress: true,
+    port: 3000,
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"]
-          }
-        }
-      }
-    ]
-  }
+        test: /.s?css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+
+        type: 'asset/resource',
+
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin(),
+      new TerserPlugin()
+    ],
+  },
+  plugins: [
+    new MinifyHtmlWebpackPlugin({
+      src: "./src/html",
+      dest: "./public",
+      rules: {
+        collapseBooleanAttributes: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true,
+        removeComments: true,
+        minifyJS: true,
+      },
+    }),
+    new MiniCssExtractPlugin(),
+    new PurgecssPlugin({
+        paths: glob.sync(`${PATHS.src}/**/*`,  { nodir: true }),
+      }),
+  ],
 };
