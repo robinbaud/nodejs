445df30d59a7ce3fd683bdda44b5cd08665b7a92
robinbaud <72597879+robinbaud@users.noreply.github.com>
2021-10-07 14:25:53 +0200

    wingardium leviosa

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import retrieveContent from './query.js';
 
-const url = 'https://api.gill-cote-bistro.fr/';
 async function showContent() {
   try {
     const content = await retrieveContent();
@@ -9,12 +8,6 @@ async function showContent() {
     elt.innerHTML = content.join('<br />');
 
     document.getElementsByTagName('body')[0].appendChild(elt);
-
-    fetch(url + 'specials?_sort=id:asc')
-    .then((resp) => resp.json())
-    .then(function(data){
-      console.log(data)
-    })
     
   } catch (e) {
     console.log('Error', e);
--- src/query.js
+++ src/query.js
@@ -1,7 +1,23 @@
 export default async function retrieveContent() {
     const url = "https://baconipsum.com/api/?type=all-meat&paras=2&start-with-lorem=1";
-  
     const response = await fetch(url);
     return response.json();
-  }
+}
   
+export default async function grill(){
+  const url = 'https://api.gill-cote-bistro.fr/specials?_sort=id:asc';
+  const reponse = await fetch(url);
+  return reponse.json();
+}
+
+export default async function grill(){
+  const url = 'https://api.gill-cote-bistro.fr/suggestions?_sort=id:asc';
+  const reponse = await fetch(url);
+  return reponse.json();
+}
+
+export default async function grill(){
+  const url = 'https://api.gill-cote-bistro.fr/histoires?_sort=id:asc';
+  const reponse = await fetch(url);
+  return reponse.json();
+}
